b511bdd83d3f503c93321606a17a887f9d692d9c
unknown <salimiyahya549@gmail.com>
2022-06-23 22:03:22 +0300

    throw more specific error messages when logging fail

--- server/src/config/local.strategy.ts
+++ server/src/config/local.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 
@@ -12,6 +12,14 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
 
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.login(email, password);
+    if (!user) {
+      throw new UnauthorizedException('Email or password is incorrect');
+    }
+    if (!user.emailConfirmed) {
+      throw new UnauthorizedException(
+        'User has not confirmed his/her email address',
+      );
+    }
     return user;
   }
 }
--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -14,15 +14,7 @@ export class AuthService {
   async login(email: string, password: string): Promise<UserDocument> {
     try {
       const user = await this.userModal.findOne({ email: email });
-      if (!user.emailConfirmed) {
-        throw new UnauthorizedException(
-          'User has not activated their email address',
-        );
-      }
       if (user && (await bcrypt.compare(password, user.password))) {
-        /*recording lastloggedin in UTC, the frontend developer is responsible for showing
-          local time to the user using the timezoneOffset-field of user's profile 
-        */
         user.lastLoggedIn = new Date();
         return await user.save();
       }
