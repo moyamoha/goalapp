e53811ab55a8a07739f2be0194d81250f8ba4c59
Yahya Salimi <yahya.salimi@eduhouse.fi>
2022-09-25 07:07:02 +0300

    edit personal website address

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -5,7 +5,7 @@ import globalStyles from "@styles/Globals.module.css";
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
-			<a href="https://yahyasalimi.netlify.app" className={globalStyles.link}>
+			<a href="https://yahyasalimi.com" className={globalStyles.link}>
 				Yahya Salimi
 			</a>
 		</footer>
