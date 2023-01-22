d3d0d6db9648d38cb69a37c6f089cfb478ca1463
Yahya Salimi <yahya.salimi@eduhouse.fi>
2023-01-22 17:14:59 +0200

    Changes on 2023-01-22 17:14:59.541437

--- server/src/types/custom.ts
+++ server/src/types/custom.ts
@@ -2,7 +2,6 @@ import { Request } from 'express';
 import { GoalDocument } from 'src/schemas/goal.schema';
 import { TodoDocument } from 'src/schemas/todo.schema';
 import { UserDocument } from 'src/schemas/user.schema';
-import { Document } from 'mongoose';
 
 export interface CustomRequest extends Request {
   user?: UserDocument;
