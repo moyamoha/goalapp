13df4aac6433658e946f7d49ff4d0bd41627c7fe
Yahya Salimi <yahya.salimi@eduhouse.fi>
2023-12-25 15:55:10 +0200

    Changes on 2023-12-25 15:55:10.369767

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -7,7 +7,7 @@ import store, { persistor } from "@state/store";
 
 import "@styles/globals.css";
 
-axios.defaults.baseURL = "https://goalapp.onrender.com/";
+axios.defaults.baseURL = "https://ambitious.onrender.com/";
 axios.interceptors.request.use((request) => {
   const token = localStorage.getItem("accessToken");
   if (token) {
