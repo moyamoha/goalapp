e189bb9e6f047735e12d235862acfd836a7acdff
Yahya Salimi <yahya.salimi@eduhouse.fi>
2023-12-27 20:54:29 +0200

    fix an application error related to quotes2

--- client/components/ZenQuote.tsx
+++ client/components/ZenQuote.tsx
@@ -4,34 +4,34 @@ import { useAppDispatch, useAppSelector } from "state/hooks";
 import qStyles from "@styles/Quotes.module.css";
 
 export default function ZenQuote() {
-	const quotes = useAppSelector((s) => s.quotes);
-	const [randomInd, setRandomInd] = useState(0);
+  const quotes = useAppSelector((s) => s.quotes);
+  const [randomInd, setRandomInd] = useState(0);
 
-	const generateRandomInd = useCallback(() => {
-		const randomNum = quotes.quotes
-			? Math.floor(Math.random() * quotes.quotes?.length - 1)
-			: 0;
-		setRandomInd(randomNum as number);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+  const generateRandomInd = useCallback(() => {
+    const randomNum = quotes.quotes
+      ? Math.floor(Math.random() * quotes.quotes?.length - 1)
+      : 0;
+    setRandomInd(randomNum as number);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-	useEffect(function () {
-		generateRandomInd();
-		const interval = setInterval(() => {
-			generateRandomInd();
-		}, 20000);
-		return () => clearInterval(interval);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+  useEffect(function () {
+    generateRandomInd();
+    const interval = setInterval(() => {
+      generateRandomInd();
+    }, 20000);
+    return () => clearInterval(interval);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-	return quotes.quotes ? (
-		<div className={qStyles.quoteContainer}>
-			<p>
-				{quotes.quotes[randomInd].text}{" "}
-				<i style={{ color: "yellow" }}>{quotes.quotes[randomInd].author}</i>
-			</p>
-		</div>
-	) : (
-		<></>
-	);
+  return quotes.quotes && quotes.quotes.length > 0 ? (
+    <div className={qStyles.quoteContainer}>
+      <p>
+        {quotes.quotes[randomInd].text}{" "}
+        <i style={{ color: "yellow" }}>{quotes.quotes[randomInd].author}</i>
+      </p>
+    </div>
+  ) : (
+    <></>
+  );
 }
